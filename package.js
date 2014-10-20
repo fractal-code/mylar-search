6241b52c394938965f6354ad0a5abcf4622ebb64
Yaser Jarrar <yaser@grout.io>
2014-10-20 17:57:51 +0400

    Bump version to 0.1.1

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
     summary: "Multi-key search over encrypted data",
     name: "mylar:search",
-    version: '0.1.0',
+    version: '0.1.1',
     git: "https://github.com/gliesesoftware/mylar-search.git"
 });
 
