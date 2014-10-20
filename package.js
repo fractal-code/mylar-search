74c5a6e52a838234381d328b94654071c129ed0a
Yaser Jarrar <yaser@grout.io>
2014-10-20 17:52:20 +0400

    Remove redundant NaCl code from package def

--- package.js
+++ package.js
@@ -6,8 +6,6 @@ Package.describe({
 });
 
 Package.onUse(function (api) {
-    const USE_NACL = false;
-
     api.use(['underscore', 'json', 'tracker', 'session', 'mongo', 'http',
              'mylar:timing', 'mylar:basic-crypto', 'mylar:principal'], ['client', 'server']);
 
@@ -20,40 +18,4 @@ Package.onUse(function (api) {
     api.export("MylarCrypto");
     api.export("MYLAR_USE_SEARCH");
     api.export("USE_CRYPTO_SERVER");
-        
-    if (USE_NACL) {
-      // What a hack!  Extract source_root via the exception string.
-      var source_root;
-      var magic_string = '@@nonexistent_file@@';
-      try {
-        Package._require(magic_string);
-      } catch (err) {
-        var msg = err.message;
-        var first_quote = msg.indexOf("'");
-        var path_plus = msg.substr(first_quote + 1);
-        source_root = path_plus.substr(0, path_plus.indexOf(magic_string));
-      }
-
-      var fs = Npm.require('fs');
-      function scan_dir(d) {
-        var files = fs.readdirSync(source_root + d);
-        for (var i = 0; i < files.length; i++) {
-          var f = files[i];
-          var df = d + '/' + files[i];
-          var st = fs.statSync(source_root + df);
-          if (st.isDirectory() && f !== 'main' && f !== 'src') {
-            scan_dir(df);
-          }
-
-          if (st.isFile()) {
-            api.addFiles(df, 'client');
-          }
-        }
-      }
-
-      if (fs.existsSync(source_root + 'crypto_ext')) {
-        scan_dir('crypto_ext');
-      }
-    }
-
 });
